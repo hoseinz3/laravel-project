f0e1932030454deed9c2701604c842b05c51fd9a
hosein <mhs.abedi@gmail.com>
2015-11-23 19:27:34 +0330

    i insert some bug to code

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('about',function(){
-	return view('about');
+	return view('abuot');
 })
 
 // create about page and so on
