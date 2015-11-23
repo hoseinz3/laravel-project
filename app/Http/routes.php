d9bc88008af23fef82ad696c53fcf6e8273b3f69
hosein <mhs.abedi@gmail.com>
2015-11-23 19:39:41 +0330

    i fixed the bug

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -15,7 +15,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 Route::get('about',function(){
-	return view('abuot');
+	return view('about');
 })
 
 // create about page and so on
